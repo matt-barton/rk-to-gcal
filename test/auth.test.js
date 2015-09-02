e3f47698cfd79bf040d26c25e4d8286f6c6d9049
matt-barton <matt@mattbarton.org>
2015-09-02 11:03:49 +0100

    Completed dev of auth.requiresAuthentication

--- test/auth.test.js
+++ test/auth.test.js
@@ -11,9 +11,6 @@ describe ('auth', function () {
 			'Then the result.redirect method is invoked', function (done) {
 
 			var redirectCalled = false
-			var mockRequest = {
-
-			}
 			var mockResult = {
 				redirect: function() {
 					redirectCalled = true
@@ -23,9 +20,109 @@ describe ('auth', function () {
 
 			var auth = require ('../lib/auth')
 
-			auth.requiresAuthentication(mockRequest, mockResult)
+			auth.requiresAuthentication({}, mockResult)
 
 			redirectCalled.should.equal(true)
 		})
+
+		it ('Given a request session doesn\'t exist ' +
+			'When authentication status is tested ' +
+			'Then the result.redirect method is used to redirect to /auth/login', function (done) {
+
+			var mockResult = {
+				redirect: function(url) {
+					url.should.equal('/auth/login')
+					done()
+				}
+			}
+
+			var auth = require ('../lib/auth')
+
+			auth.requiresAuthentication({}, mockResult)
+		})
+
+		it ('Given a request session is null ' +
+			'When authentication status is tested ' +
+			'Then the result.redirect method is used to redirect to /auth/login', function (done) {
+
+			var mockResult = {
+				redirect: function(url) {
+					url.should.equal('/auth/login')
+					done()
+				}
+			}
+			var mockRequest = {
+				session: null
+			}
+			var auth = require ('../lib/auth')
+
+			auth.requiresAuthentication(mockRequest, mockResult)
+		})
+
+
+		it ('Given a request session exists ' +
+			'And the session contains no user data ' +
+			'When authentication status is tested ' +
+			'Then the result.redirect method is used to redirect to /auth/login', function (done) {
+
+			var mockResult = {
+				redirect: function(url) {
+					url.should.equal('/auth/login')
+					done()
+				}
+			}
+			var mockRequest = {
+				session: {}
+			}
+			var auth = require ('../lib/auth')
+
+			auth.requiresAuthentication(mockRequest, mockResult)
+		})
+
+		it ('Given a request session exists ' +
+			'And the session contains a user with no identity ' +
+			'When authentication status is tested ' +
+			'Then the result.redirect method is used to redirect to /auth/login', function (done) {
+
+			var mockResult = {
+				redirect: function(url) {
+					url.should.equal('/auth/login')
+					done()
+				}
+			}
+			var mockRequest = {
+				session: {
+					user: {}
+				}
+			}
+			var auth = require ('../lib/auth')
+
+			auth.requiresAuthentication(mockRequest, mockResult)
+		})
+
+		it ('Given a request session exists ' +
+			'And the session contains a user identity ' +
+			'When authentication status is tested ' +
+			'Then the next method is invoked', function (done) {
+
+			var nextInvoked = false
+			function mockNext () {
+				nextInvoked = true
+				done()
+			}
+			var mockRequest = {
+				session: {
+					user: {
+						identity: 'an identity'
+					}
+				}
+			}
+			var auth = require ('../lib/auth')
+
+			auth.requiresAuthentication(mockRequest, {}, mockNext)
+
+			nextInvoked.should.equal(true)
+		})
+
 	})
 })
