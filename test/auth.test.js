9afb37bf0ac9bf7e0f916f16c0ddc2ff19c87d50
matt-barton <matt@mattbarton.org>
2015-09-03 16:09:22 +0100

    Implemented auth.login

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,8 +1,11 @@
 'use strict'
 
 var should = require('should')
+var bcrypt = require('bcrypt-nodejs')
 
 describe ('auth', function () {
+
+	var Auth = require('../lib/auth')
 
 	describe ('requiresAuthentication', function () {
 
@@ -18,7 +21,7 @@ describe ('auth', function () {
 				}
 			}
 
-			var auth = require ('../lib/auth')
+			var auth = new Auth
 
 			auth.requiresAuthentication({}, mockResult)
 
@@ -36,7 +39,7 @@ describe ('auth', function () {
 				}
 			}
 
-			var auth = require ('../lib/auth')
+			var auth = new Auth
 
 			auth.requiresAuthentication({}, mockResult)
 		})
@@ -54,7 +57,8 @@ describe ('auth', function () {
 			var mockRequest = {
 				session: null
 			}
-			var auth = require ('../lib/auth')
+
+			var auth = new Auth
 
 			auth.requiresAuthentication(mockRequest, mockResult)
 		})
@@ -74,7 +78,8 @@ describe ('auth', function () {
 			var mockRequest = {
 				session: {}
 			}
-			var auth = require ('../lib/auth')
+
+			var auth = new Auth
 
 			auth.requiresAuthentication(mockRequest, mockResult)
 		})
@@ -95,7 +100,8 @@ describe ('auth', function () {
 					user: {}
 				}
 			}
-			var auth = require ('../lib/auth')
+
+			var auth = new Auth
 
 			auth.requiresAuthentication(mockRequest, mockResult)
 		})
@@ -117,12 +123,139 @@ describe ('auth', function () {
 					}
 				}
 			}
-			var auth = require ('../lib/auth')
 
+			var auth = new Auth
 			auth.requiresAuthentication(mockRequest, {}, mockNext)
 
 			nextInvoked.should.equal(true)
 		})
+
+	})
+
+	describe ('login', function() {
+
+		it ('Given a username is not supplied ' +
+			'When logging in ' +
+			'Then an error is invoked', function (done) {
+
+			var auth = new Auth
+
+			auth.login(null, 'PASSWORD', function(e) {
+				e.should.be.type('object')
+				e.message.should.equal('Username is required')
+				done()
+			})
+		})
+
+		it ('Given a password is not supplied ' +
+			'When logging in ' +
+			'Then an error is invoked', function (done) {
+
+			var auth = new Auth
+
+			auth.login('USERNAME', null, function(e) {
+				e.should.be.type('object')
+				e.message.should.equal('Password is required')
+				done()
+			})
+		})
+
+		it ('Given a username and password are supplied ' +
+			'When logging in ' +
+			'Then a user record is retrieved for the username', function (done) {
+
+			var mockDb = {
+				getUser: function(username) {
+					username.should.equal('USERNAME')
+					done()
+				}
+			}
+			var auth = new Auth(mockDb)
+
+			auth.login('USERNAME', 'PASSWORD', function() {})
+		})
+
+		it ('Given an error occurs ' +
+			'When logging in ' +
+			'Then the error is bubbled up', function (done) {
+
+			var mockDb = {
+				getUser: function(username, cb) {
+					cb(new Error ('AN ERROR'))
+				}
+			}
+			var auth = new Auth(mockDb)
+
+			auth.login('USERNAME', 'PASSWORD', function(e) {
+				e.should.be.type('object')
+				e.message.should.equal('AN ERROR')
+				done()
+			})
+		})
+
+		it ('Given no user account is found for the supplied username ' +
+			'When logging in ' +
+			'Then a negative response is returned', function (done) {
+
+			var mockDb = {
+				getUser: function(username, cb) {
+					cb(null, [])
+				}
+			}
+			var auth = new Auth(mockDb)
+
+			auth.login('USERNAME', 'PASSWORD', function(e, result) {
+				should.not.exist(e)
+				result.should.equal(false)
+				done()
+			})
+		})
+
+		it ('Given a user account is found for the supplied username ' +
+			'And the password supplied does not match the stored password ' +
+			'When logging in ' +
+			'Then a negative response is returned', function (done) {
+
+			var mockDb = {
+				getUser: function(username, cb) {
+					cb(null, [{
+						pwHash: bcrypt.hashSync('POTATOES')
+					}])
+				}
+			}
+			var auth = new Auth(mockDb)
+
+			auth.login('USERNAME', 'PASSWORD', function(e, result) {
+				should.not.exist(e)
+				result.should.equal(false)
+				done()
+			})
+		})
+
+
+		it ('Given a user account is found for the supplied username ' +
+			'And the password supplied matches the stored password ' +
+			'When logging in ' +
+			'Then the stored user record is returned', function (done) {
+
+			var mockUser = {
+				identity: 'USERNAME',
+				pwHash: bcrypt.hashSync('PASSWORD')
+			}
+			var mockDb = {
+				getUser: function(username, cb) {
+					cb(null, [mockUser])
+				}
+			}
+			var auth = new Auth(mockDb)
+
+			auth.login('USERNAME', 'PASSWORD', function(e, result) {
+				should.not.exist(e)
+				result.should.equal(mockUser)
+				done()
+			})
+		})
+
 
 	})
 })
