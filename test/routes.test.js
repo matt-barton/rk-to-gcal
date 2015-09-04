73241dab15171d801c8849a330cb2256bdc3711b
matt-barton <matt@mattbarton.org>
2015-09-04 23:44:19 +0100

    Further bdd of user registration

--- test/routes.test.js
+++ test/routes.test.js
@@ -2,10 +2,7 @@
 
 var should = require ('should')
 var mockExpressApp
-
-var mockUtils = {
-	buildUrl: function () {}
-}
+var utils = require('../lib/utils')
 
 function assertHandlerExists (done, path, method, useMiddleware, useAuthMiddleware) {
 
@@ -40,9 +37,7 @@ function assertHandlerExists (done, path, method, useMiddleware, useAuthMiddlewa
 			done()
 		}
 	}
-	mockExpressApp.lib = {
-		auth: mockAuth
-	}
+	mockExpressApp.lib.auth = mockAuth
 
 	var routes = require('../lib/routes')(mockExpressApp)
 	correctUri.should.equal(true)
@@ -57,7 +52,8 @@ beforeEach(function() {
 		post: function () {},
 		use: function () {},
 		lib: {
-			auth: {}
+			auth: {},
+			utils: utils
 		}
 	}
 })
@@ -458,6 +454,306 @@ describe ('login', function () {
 
 		var routes = require('../lib/routes')(mockExpressApp)
 	})
+
+})
+
+describe ('register', function () {
+
+	it('When routes are set up ' +
+		'Then a GET handler is set up for /auth/register', function (done) {
+
+		assertHandlerExists(done, '/auth/register', 'GET')
+	})
+
+	it('When the GET /auth/register routes is requested ' +
+		'Then the register view is rendered', function (done) {
+
+		var mockResponseObject = {
+			render: function (viewName) {
+				viewName.should.equal('register')
+				done()
+			}
+		}
+
+		mockExpressApp.get = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (null, mockResponseObject)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it('When routes are set up ' +
+		'Then a POST handler is set up for /auth/register', function (done) {
+
+		assertHandlerExists(done, '/auth/register', 'POST')
+	})
+
+
+	it ('Given the request data does not contain an identity field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Email address is required')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the request data does not contain a password field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password is required')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY'
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the request data does not contain a password repeat field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password is required twice')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY',
+				password: 'PASSWORD'
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the request data contains only whitespace in the identity field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Email address is required')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: '   '
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the request data contains only whitespace in the password field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password is required')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY',
+				password: '    '
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+
+	it ('Given the request data contains only whitespace in the password repeat field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password is required twice')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY',
+				password: 'PASSWORD',
+				pwRepeat: '   '
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the supplied password and repeat fields do not match ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password and Repeat do not match')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY',
+				password: 'PASSWORD',
+				pwRepeat: 'POTATOES'
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the supplied password and repeat fields do match ' +
+		'And the password field is less than 10 characters in length ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password should be 10 characters or longer')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITY',
+				password: 'PASSWORD',
+				pwRepeat: 'PASSWORD'
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
+	it ('Given the supplied password and repeat fields do match ' +
+		'And the password field is the same as the identity field ' +
+		'When the /auth/register route is posted to ' +
+		'Then the register view is rendered with an error message', function (done) {
+
+		var mockResponse = {
+			render: function (viewName, data) {
+				viewName.should.equal('register')
+				data.should.be.type('object')
+				data.registrationError.should.equal('Password cannot be the same as your email address')
+				done()
+			}
+		}
+
+		var mockRequest = {
+			body: {
+				identity: 'IDENTITYIDENTITY',
+				password: 'IDENTITYIDENTITY',
+				pwRepeat: 'IDENTITYIDENTITY'
+			}
+		}
+
+		mockExpressApp.post = function (uri, cb) {
+			if (uri == '/auth/register') {
+				cb (mockRequest, mockResponse)
+			}
+		}
+
+		var routes = require('../lib/routes')(mockExpressApp)
+	})
+
 
 })
 })
--- test/utils.test.js
+++ test/utils.test.js
@@ -5,59 +5,119 @@ var utils = require('../lib/utils')
 
 describe('utils', function() {
 
-	describe('buildUrl', function() {
+describe('buildUrl', function() {
 
-		it ('Given a base url ' +
-			'and no query parameters '+
-			'When a url is built ' +
-			'Then the base url is returned', function(done){
-			var baseUrl = '<base url>'
-			var test = utils.buildUrl(baseUrl, {})
-			test.should.equal(baseUrl)
-			done()
-		})
-
-		it ('Given a base url ' +
-			'and one parameter ' +
-			'When a url is built ' +
-			'Then a url containing the parameter is returned', function (done) {
-			var baseUrl = 'http://example.com'
-			var parameters = {
-				'param1': 'value1'
-			}
-			var test = utils.buildUrl(baseUrl, parameters)
-			test.should.equal('http://example.com?param1=value1')
-			done();
-		})
-
-		it ('Given a base url ' +
-			'and many parameters ' +
-			'When a url is built ' +
-			'Then a url containing all the parameters is returned', function (done) {
-			var baseUrl = 'http://example.com'
-			var parameters = {
-				'p1': 'v1',
-				'p2': 'v2',
-				'p3': 'v3'				
-			}
-			var test = utils.buildUrl(baseUrl, parameters)
-			test.should.equal('http://example.com?p1=v1&p2=v2&p3=v3')
-			done();
-		})
-
-
-		it ('Given a parameter with special characters ' +
-			'When a url is built ' +
-			'Then a url containing url-encoded characters is returned', function (done) {
-			var baseUrl = 'http://example.com'
-			var parameters = {
-				'p1': 'v 1',
-				'p2': 'v&2'				
-			}
-			var test = utils.buildUrl(baseUrl, parameters)
-			test.should.equal('http://example.com?p1=v%201&p2=v%262')
-			done();
-		})
-
+	it ('Given a base url ' +
+		'and no query parameters '+
+		'When a url is built ' +
+		'Then the base url is returned', function(done){
+		var baseUrl = '<base url>'
+		var test = utils.buildUrl(baseUrl, {})
+		test.should.equal(baseUrl)
+		done()
 	})
+
+	it ('Given a base url ' +
+		'and one parameter ' +
+		'When a url is built ' +
+		'Then a url containing the parameter is returned', function (done) {
+		var baseUrl = 'http://example.com'
+		var parameters = {
+			'param1': 'value1'
+		}
+		var test = utils.buildUrl(baseUrl, parameters)
+		test.should.equal('http://example.com?param1=value1')
+		done()
+	})
+
+	it ('Given a base url ' +
+		'and many parameters ' +
+		'When a url is built ' +
+		'Then a url containing all the parameters is returned', function (done) {
+		var baseUrl = 'http://example.com'
+		var parameters = {
+			'p1': 'v1',
+			'p2': 'v2',
+			'p3': 'v3'				
+		}
+		var test = utils.buildUrl(baseUrl, parameters)
+		test.should.equal('http://example.com?p1=v1&p2=v2&p3=v3')
+		done()
+	})
+
+
+	it ('Given a parameter with special characters ' +
+		'When a url is built ' +
+		'Then a url containing url-encoded characters is returned', function (done) {
+		var baseUrl = 'http://example.com'
+		var parameters = {
+			'p1': 'v 1',
+			'p2': 'v&2'				
+		}
+		var test = utils.buildUrl(baseUrl, parameters)
+		test.should.equal('http://example.com?p1=v%201&p2=v%262')
+		done()
+	})
+})
+
+describe('validString', function() {
+
+	it ('Given the variable to test is undefined ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString()
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is null ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString(null)
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is an array ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString([])
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is an object ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString(new Object)
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is an empty string ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString('')
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is a string containing just whitespace ' +
+		'When testing string validity ' +
+		'Then false is returned', function (done) {
+		var result = utils.validString('   ')
+		result.should.equal(false)
+		done()		
+	})
+
+	it ('Given the variable to test is a string non-whitespace characters ' +
+		'When testing string validity ' +
+		'Then true is returned', function (done) {
+		var result = utils.validString('  x ')
+		result.should.equal(true)
+		done()		
+	})
+
+
+})
 })
