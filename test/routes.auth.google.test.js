e5c502e49dcec8f4af36780440e17c3b75bc5f5a
matt-barton <matt@mattbarton.org>
2015-09-29 16:56:15 +0100

    Implemented /auth/google route

--- test/routes.auth.google.test.js
+++ test/routes.auth.google.test.js
@@ -56,7 +56,31 @@ describe('routes/auth/google', function () {
 			}
 		}
 		require('../lib/routes')(mockExpressApp)
+	})
 
+	it ('Given the user not connected to google\n' +
+		'WHen the /auth/google route is requested\n' +
+		'Then the google auth redirect is invoked', function (done) {
+		
+		var request = {
+			session: {
+				user: {
+				}
+			}
+		}
+		var response = {}
+		mockExpressApp.get = function (uri, middleware, cb) {
+			if (uri == '/auth/google') {
+				cb (request, response)
+			}
+		}
+		mockExpressApp.lib.google = {
+			redirectToAuth: function (responseObject) {
+				responseObject.should.equal(response)
+				done()
+			}
+		}
+		require('../lib/routes')(mockExpressApp)
 	})
 
 })
